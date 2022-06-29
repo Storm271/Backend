4095880569b3bf63709ce4177217048007e146cc
27010-34-11 <27010-34-11@exchange.ad.afpanet>
2022-06-29 16:39:24 +0200

    3e jour ECF PHP

--- Menuiz_Ecommerce/Model/updateRetourModel.php
+++ Menuiz_Ecommerce/Model/updateRetourModel.php
@@ -1,38 +1,65 @@
 <?php
-function getDatabaseConnexion()
-{
-    try {
-        $user = "root";
-        $pass = "";
-        $pdo = new PDO('mysql:host=localhost;dbname=menuiz2', $user, $pass);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch (PDOException $e) {
-        print "Erreur !: " . $e->getMessage() . "<br/>";
-        die();
-    }
-}
 
-function updateRetour($status, $commentaire)
+class ModeleUpdateRetour
 {
-    try {
-        $con = getDatabaseConnexion();
-        $sql = "INSERT INTO t_d_user_usr (SVL_STATUS, SVF_COMM) 
-                VALUES ('$status', '$commentaire' )";
-        $con->exec($sql);
-    } catch (PDOException $e) {
-        echo $sql . "<br>" . $e->getMessage();
+    private $idc;
+    private function connexion()
+    {
+        $this->idc = new PDO("mysql:host=localhost;  dbname=menuiz2", 'root', '');
     }
-}
+    
 
-function update($nom, $prenom, $email, $password, $pseudo)
-{
-    try {
-        $con = getDatabaseConnexion();
-        $sql = "INSERT INTO t_d_user_usr (USR_LASTNAME, USR_FIRSTNAME, USR_MAIL, USR_PASSWORD, username) 
-                VALUES ('$nom', '$prenom', '$email', '$password', '$pseudo' )";
-        $con->exec($sql);
-    } catch (PDOException $e) {
-        echo $sql . "<br>" . $e->getMessage();
+
+
+    public function updateRetour($statu, $identifiant, $commentaire)
+    {
+        $this->connexion();
+        $query = "UPDATE t_d_savfile_svf 
+        SET SVL_STATUS = '$statu',
+        Usr_ID = '$identifiant' ,
+        SVF_COMM ='$commentaire'
+        where SVF_Product= 6"
+;
+
+        $stmt = $this->idc->prepare($query);
+        $stmt->execute([
+            ':statu' => $statu,
+            ':id' => $identifiant,
+            ':commentaire' => $commentaire
+        ]);
+    }
+    
+    
+
+
+
+
+
+    public function RecupProduit($id)
+    {
+        $this->connexion();
+        $res = $this->idc->prepare("SELECT SVF_ID, SVF_COMM, SVL_STATUS, SVF_CREATIONTIME, OHR_NUMBER,SVF_Product, CONCAT(USR_FIRSTNAME, ' ', USR_LASTNAME) as nom, t_d_user_usr.USR_ID FROM t_d_user_usr
+    join t_d_savfile_svf on t_d_savfile_svf.Usr_ID = t_d_user_usr.Usr_ID
+    join t_d_savdetails_svl on t_d_savdetails_svl.SVL_ID = t_d_savfile_svf.SVL_ID
+    join t_d_orderheader_ohr on t_d_orderheader_ohr.OHR_ID = t_d_savfile_svf.OHR_ID 
+    where SVF_Product= ".$id."");
+        $res->execute();
+        return $res;
+    }
+
+    public function RecupStatus()
+    {
+        $this->connexion();
+        $res = $this->idc->prepare("SELECT  SVL_STATUS FROM t_d_savdetails_svl");
+        $res->execute();
+        return $res;
+    }
+
+    public function RecupTech()
+    {
+        $this->connexion();
+        $res = $this->idc->prepare("SELECT  Usr_ID FROM t_d_savfile_svf");
+        $res->execute();
+        return $res;
     }
 }
